f726e5d589136fee3c3c13452080e7d54b4758da
Vistor25 <victorvoronko@gmail.com>
2016-10-23 02:02:05 +0300

    add

---
+++ Task1/SearchingIndexLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class SearchingIndexLogic
+    {
+        public static int Search(int [] array)
+        {
+            int left = 0;
+            int right = array.Sum() - array[0];
+            int i;
+            for (i = 0; left != right&&i<array.Length; i++)
+            {
+                left += array[i];
+                right -= array[i + 1];
+            }
+            return left == right ? i : -1;
+        }
+    }
+}
---
+++ Task2/StringSplit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class StringSplit
+    {
+        public static string StringSplitting(string a, string b)
+        {
+            string c = a + b;
+            char[] cSplitted = c.ToCharArray();
+            Array.Sort(cSplitted);
+            var result = new StringBuilder(cSplitted[0].ToString());
+            for(int i=0; i < cSplitted.Length-1; i++)
+            {
+                if (cSplitted[i] != cSplitted[i + 1])
+                {
+                    result.Append(cSplitted[i + 1]);
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
